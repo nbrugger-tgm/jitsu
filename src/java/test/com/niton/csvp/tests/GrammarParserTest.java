0c4ddaea054df048fd55bbadaab611efe3d88434
Nils Brugger <nbrugger@student.tgm.ac.at>
2019-06-12 16:35:05 +0200

    Better Exception handling for Test Case

--- src/java/test/com/niton/csvp/tests/GrammarParserTest.java
+++ src/java/test/com/niton/csvp/tests/GrammarParserTest.java
@@ -24,24 +24,24 @@ class GrammarParserTest {
 	}
 
 	@Test
-	void parseWellTokens() throws ParsingException {
-		ArrayList<String> tokens = new ArrayList<>();
-		tokens.add("LINE_END");
-		tokens.add("IDENTIFYER");
-		tokens.add("EQ");
-		tokens.add("WHITESPACE");
-		tokens.add("SLASH");
-		tokens.add("COMMA");
-		test("LINE_END='\\r\\n'\r\n" + 
-				"IDENTIFYER='[A-Za-z_]+'\r\n" + 
-				"EQ='[=]'\r\n" + 
-				"WHITESPACE='[ \\t]+'\r\n" + 
-				"SLASH='\\/'\r\n" + 
-				"COMMA=','", true, tokens, new ArrayList<>());
+	void parseWellTokens() {
+			ArrayList<String> tokens = new ArrayList<>();
+			tokens.add("LINE_END");
+			tokens.add("IDENTIFYER");
+			tokens.add("EQ");
+			tokens.add("WHITESPACE");
+			tokens.add("SLASH");
+			tokens.add("COMMA");
+			test("LINE_END='\\r\\n'\r\n" + 
+					"IDENTIFYER='[A-Za-z_]+'\r\n" + 
+					"EQ='[=]'\r\n" + 
+					"WHITESPACE='[ \\t]+'\r\n" + 
+					"SLASH='\\/'\r\n" + 
+					"COMMA=','", false, tokens, new ArrayList<>());
 	}
 	
 	@Test
-	void parseTokensWithComments() throws ParsingException{
+	void parseTokensWithComments() {
 		ArrayList<String> tokens = new ArrayList<>();
 		tokens.add("LINE_END");
 		tokens.add("IDENTIFYER");
@@ -63,26 +63,32 @@ class GrammarParserTest {
 				"//SOME Escaping shit\r\n" + 
 				"QUOTE='(?<!\\\\)\\\\''\r\n" + 
 				"ESCAPED_QUOTE='\\\\\\\\''\r\n" + 
-				"BACKSLASH='\\\\(?!\\')'", true, tokens, new ArrayList<>());
+				"BACKSLASH='\\\\(?!\\')'", false, tokens, new ArrayList<>());
 	}
 	
-	private static void test(String s,boolean debug,ArrayList<String> tokenNamesExpected,ArrayList<String> expectedGrammarNames) throws ParsingException {
-		Parser p = new GrammarParser();
-		if(debug) {
-			System.out.println("\tTokens : \n"+p.getT().parse(s));
-			System.out.println("\t Grammar Pure : "+((SubGrammerObject)p.parse(s)).toString(3));
-		}
-		GrammarFile file = new GrammarFile((SubGrammerObject) p.parse(s));
-		for (IgnoringTokenDefiner string : file.getHead().getTokenDefiners()) {
-			assertTrue(tokenNamesExpected.contains(string.getDefiner().getName()),"There was an unexpected element in the parsed list");
-			tokenNamesExpected.remove(string.getDefiner().getName());
-		}
-		assertEquals(0, tokenNamesExpected.size(),"Some tokens where not parsed "+tokenNamesExpected+"");
-		for (com.niton.parser.specific.grammar.gen.Grammar string : file.getGrammars()) {
-			assertTrue(expectedGrammarNames.contains(string.getName()),"There was an unexpected element in the parsed list");
-			expectedGrammarNames.remove(string.getName());
-		}
-		assertEquals(0, expectedGrammarNames.size(),"Some grammers where not parsed");
+	private static void test(String s,boolean debug,ArrayList<String> tokenNamesExpected,ArrayList<String> expectedGrammarNames) {
+		assertDoesNotThrow(()->{
+			Parser p = new GrammarParser();
+			if(debug) {
+				System.out.println("\tTokens : \n"+p.getT().parse(s));
+				System.out.println("\tGrammar Pure : "+((SubGrammerObject)p.parse(s)).toString(3));
+			}
+			GrammarFile file = new GrammarFile((SubGrammerObject) p.parse(s));
+			if(file.getHead() != null && file.getHead().getTokenDefiners() != null) {
+				for (IgnoringTokenDefiner string : file.getHead().getTokenDefiners()) {
+					assertTrue(tokenNamesExpected.contains(string.getDefiner().getName()),"There was an unexpected element in the parsed list");
+					tokenNamesExpected.remove(string.getDefiner().getName());
+				}
+			}
+			assertEquals(0, tokenNamesExpected.size(),"Some tokens where not parsed "+tokenNamesExpected+"");
+			if(file.getGrammars() != null) {
+				for (com.niton.parser.specific.grammar.gen.Grammar string : file.getGrammars()) {
+					assertTrue(expectedGrammarNames.contains(string.getName()),"There was an unexpected element in the parsed list");
+					expectedGrammarNames.remove(string.getName());
+				}
+			}
+			assertEquals(0, expectedGrammarNames.size(),"Some grammers where not parsed");
+		},"An exception ocoured on parsing");
 	}
 }
 
