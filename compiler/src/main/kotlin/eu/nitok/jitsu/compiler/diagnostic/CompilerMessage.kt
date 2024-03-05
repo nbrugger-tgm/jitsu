4cbc4754b2bda08dd96e69bbbe5c3ad32995a696
Nils Brugger <nilsbrugger.nb.nb.nb@gmail.com>
2024-03-05 11:05:38 +0100

    chore: better to string for CompilerMessages

--- compiler/src/main/kotlin/eu/nitok/jitsu/compiler/diagnostic/CompilerMessage.kt
+++ compiler/src/main/kotlin/eu/nitok/jitsu/compiler/diagnostic/CompilerMessage.kt
@@ -4,11 +4,22 @@ import eu.nitok.jitsu.compiler.parser.Locatable
 import kotlinx.serialization.Serializable
 
 @Serializable
-class CompilerMessage(
+data class CompilerMessage(
     var message: String,
     val location: Locatable,
     val hints: List<Hint> = emptyList()
 ) {
     @Serializable
-    data class Hint(val message: String, val location: Locatable)
+    data class Hint(val message: String, val location: Locatable) {
+        override fun toString(): String {
+            return "$message ($location)"
+        }
+    }
+
+    override fun toString(): String {
+        return "$message ($location) ${
+            if (hints.isNotEmpty()) "\n\tHints :\n\t\t" + hints.joinToString("\n\t\t") 
+            else ""
+        }"
+    }
 }
