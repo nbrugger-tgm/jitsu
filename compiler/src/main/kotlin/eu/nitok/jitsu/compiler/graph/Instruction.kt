fbd1c7d1c98a8ced0692a523adbb8bd49ed5f45f
Nils Brugger <nilsbrugger.nb.nb.nb@gmail.com>
2024-03-07 19:56:50 +0100

    feat: add function aware to return statement

--- compiler/src/main/kotlin/eu/nitok/jitsu/compiler/graph/Instruction.kt
+++ compiler/src/main/kotlin/eu/nitok/jitsu/compiler/graph/Instruction.kt
@@ -1,12 +1,14 @@
 package eu.nitok.jitsu.compiler.graph
 
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
 
 @Serializable
 sealed class Instruction : Element {
     @Serializable
-    data class Return(val value: Expression?): Instruction() {
+    data class Return(val value: Expression?): Instruction(), FunctionAware {
         override val children: List<Element> get() = listOfNotNull(value)
+        @Transient override lateinit var function: Function
     }
 
     @Serializable
