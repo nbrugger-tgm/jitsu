d2e709d6459f8e7eef576516999d233476fa4375
Nils Brugger <nilsbrugger.nb.nb.nb@gmail.com>
2024-03-07 16:40:37 +0100

    register rust backend

---
+++ backend/rust/src/main/kotlin/eu/nitok/jitsu/backend/rust/RustBackend.kt
@@ -0,0 +1,6 @@
+package eu.nitok.jitsu.backend.rust.eu.nitok.jitsu.backend.rust
+
+import eu.nitok.jitsu.compiler.transpile.Backend
+
+class RustBackend: Backend {
+}
--- cli/src/main/kotlin/eu/nitok/jitsu/compiler/cli/commands/Transpile.kt
+++ cli/src/main/kotlin/eu/nitok/jitsu/compiler/cli/commands/Transpile.kt
@@ -1,11 +1,24 @@
 package eu.nitok.jitsu.compiler.cli.commands
 
 import picocli.CommandLine
+import picocli.CommandLine.Mixin
+import java.io.File
+import java.util.concurrent.Callable
+
 @CommandLine.Command(
     name = "transpile",
     description = ["generates code for a target language from jitsu"]
 )
-class Transpile {
+class Transpile: Callable<List<File>> {
+    @Mixin
+    lateinit var cli: Process
     @CommandLine.Option(names = ["-b", "--backend"], description = ["The backend to compile to (llvm, rust, c, js)"])
     var backendName = "rust"
+    override fun call(): List<File> {
+        val graphs = cli.call()
+        return graphs.map { graph ->
+            val backend = BackendRegistry.create(backendName)
+            backend.transpile(graph)
+        }
+    }
 }
