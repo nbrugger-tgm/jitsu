f0328479d11067d2365fa0418fc6ace3c855a730
Nils Brugger <nilsbrugger.nb.nb.nb@gmail.com>
2024-03-07 18:54:20 +0100

    feat: type access graph

--- compiler/src/main/kotlin/eu/nitok/jitsu/compiler/graph/TypeDefinition.kt
+++ compiler/src/main/kotlin/eu/nitok/jitsu/compiler/graph/TypeDefinition.kt
@@ -4,16 +4,16 @@ import eu.nitok.jitsu.compiler.ast.Located
 import kotlinx.serialization.Serializable
 
 @Serializable
-sealed interface TypeDefinition {
-    val name: Located<String>
-
+sealed class TypeDefinition : Accessible<TypeDefinition> {
+    abstract override val name: Located<String>
+    override val accessToSelf: MutableList<in Access<TypeDefinition>> = mutableListOf()
     @Serializable
-    class Struct(
+    data class Struct(
         override val name: Located<String>,
         private val fields: MutableSet<Field>,
         val generics: List<Located<String>>,
         private val embedded: MutableSet<Lazy<Struct>> = mutableSetOf()
-    ) : TypeDefinition {
+    ) : TypeDefinition() {
         @Serializable
         data class Field(val name: kotlin.String, var mutable: kotlin.Boolean, val type: Lazy<Type>)
 
@@ -24,19 +24,19 @@ sealed interface TypeDefinition {
     data class Enum(
         override val name: Located<String>,
         val constants: List<Located<String>>
-    ) : TypeDefinition
+    ) : TypeDefinition()
 
     @Serializable
     data class Alias(
         override val name: Located<String>,
         val generics: List<Located<String>>,
         var type: Lazy<Type>
-    ) : TypeDefinition
+    ) : TypeDefinition()
 
     @Serializable
     data class Interface(
         override val name: Located<String>,
         val generics: List<Located<String>>,
         val methods: Map<kotlin.String, Located<Type.FunctionTypeSignature>>
-    ) : TypeDefinition
+    ) : TypeDefinition()
 }
