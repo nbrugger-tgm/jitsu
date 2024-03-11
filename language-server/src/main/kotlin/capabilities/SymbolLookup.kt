485655f2d32cee75e8d096f01e5d9673d7f99769
Nils Brugger <nilsbrugger.nb.nb.nb@gmail.com>
2024-03-11 23:05:40 +0100

    fix: missing typefed symbols

--- language-server/src/main/kotlin/capabilities/SymbolLookup.kt
+++ language-server/src/main/kotlin/capabilities/SymbolLookup.kt
@@ -8,57 +8,31 @@ import org.eclipse.lsp4j.SymbolKind
 import range
 
 
-private fun TypeDefinition.documentSymbols(children: Iterable<DocumentSymbol>): List<DocumentSymbol> {
-    return when (this) {
-        is TypeDefinition.Enum -> {
-            listOf(
-                DocumentSymbol(
-                    name.value,
-                    SymbolKind.Enum,
-                    range(name.location),
-                    range(name.location),
-                    "enum $name (${constants.size} options)",
-                    children.toList()
-                )
-            )
-        }
+private fun TypeDefinition.documentSymbols(children: Iterable<DocumentSymbol>): List<DocumentSymbol> = listOf(
+    DocumentSymbol(
+        name.value,
+        symbolKind(),
+        range(name.location),
+        range(name.location),
+        null,
+        children.toList()
+    )
+)
 
-        is TypeDefinition.Interface -> listOf(
-            DocumentSymbol(
-                name.value,
-                SymbolKind.Interface,
-                range(name.location),
-                range(name.location),
-                null,
-                children.toList()
-            )
-        )
 
-        is TypeDefinition.Alias -> listOf(
-            DocumentSymbol(
-                name.value,
-                this.type.value.resolveTypeKind(),
-                range(name.location),
-                range(name.location),
-                null,
-                children.toList()
-            )
-        )
-
-        is TypeDefinition.Struct -> TODO()
-    }
-}
 private fun TypeDefinition.symbolKind() = when (this) {
     is TypeDefinition.Enum -> SymbolKind.Enum
     is TypeDefinition.Interface -> SymbolKind.Interface
     is TypeDefinition.Alias -> type.value.resolveTypeKind()
     is TypeDefinition.Struct -> SymbolKind.Struct
 }
+
 private fun Type.resolveTypeKind(): SymbolKind? {
     return when (this) {
         is Type.Float,
         is Type.Int,
         is Type.UInt -> SymbolKind.Number
+
         is Type.Value -> SymbolKind.Constant
         is Type.Array -> SymbolKind.Array
         Type.Boolean -> SymbolKind.Boolean
@@ -74,10 +48,7 @@ fun JitsuFile.documentSymbols(): List<DocumentSymbol> {
         if (node !is Accessible<*>) return@mapTree children;
         when (node) {
             is Function -> node.documentSymbols(children)?.let { listOf(it) } ?: children
-            is TypeDefinition.Alias -> TODO()
-            is TypeDefinition.Enum -> TODO()
-            is TypeDefinition.Interface -> TODO()
-            is TypeDefinition.Struct -> TODO()
+            is TypeDefinition -> node.documentSymbols(children)
             is Variable -> listOf(
                 DocumentSymbol(
                     node.name.value,
@@ -88,6 +59,7 @@ fun JitsuFile.documentSymbols(): List<DocumentSymbol> {
                     children.toList()
                 )
             )
+
             is TypeDefinition.Enum.Constant -> listOf(
                 DocumentSymbol(
                     node.name.value,
