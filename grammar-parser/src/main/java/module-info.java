ff8cc6c60f8721ca8ce21e092dbd24afee996e15
Nils Brugger <nilsbrugger.nb.nb.nb@gmail.com>
2025-02-26 16:12:31 +0100

    fix: add missing lombok declaration

--- grammar-parser/src/main/java/module-info.java
+++ grammar-parser/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module jainparse.grammarparser {
 	requires org.jetbrains.annotations;
     requires transitive jainparse.parser;
+    requires static lombok;
 
     exports com.niton.parsers.grammar;
 }
