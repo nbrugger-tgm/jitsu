9f98ffdf958fe44fb1ca45c3fc063b710ad6ff3f
Nils Brugger <nilsbrugger.nb.nb.nb@gmail.com>
2025-12-28 02:06:55 +0100

    refactor: move ast to parser package

---
+++ compiler/src/main/kotlin/eu/nitok/jitsu/compiler/parser/ast/BiOperator.kt
@@ -0,0 +1,22 @@
+package eu.nitok.jitsu.compiler.ast
+
+enum class BiOperator(val rune: String,val functionName: String) {
+    GREATER_EQUAL(">=", "greater_or_equal_to"),
+    LESS_EQUAL("<=", "less_or_equal_to"),
+    AND("&&","logical_and"),
+    OR("||","logical_or"),
+    ADDITION("+","plus"),
+    SUBTRACTION("-","minus"),
+    MULTIPLICATION("*","multiply_by"),
+    DIVISION("/","divide_by"),
+    MODULO("%","modulo"),
+    GREATER(">","greater_than"),
+    LESS("<","less_than"),
+    EQUALS("==","equals");
+
+    companion object {
+        fun byRune(rune: String): BiOperator? {
+            return entries.find { it.rune == rune }
+        }
+    }
+}
