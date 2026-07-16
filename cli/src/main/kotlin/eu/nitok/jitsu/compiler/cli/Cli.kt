8deeba06cfe5794148e6f61dbd774b1624200c7a
Nils Brugger <nilsbrugger.nb.nb.nb@gmail.com>
2026-07-16 05:50:48 +0200

    chores: remove CLI
    for now the CLI does not provide any value and needs to be adapted to the constantly changing api surface of the compiler. Later on we want to have it so we leave the base but all comands are gone

--- cli/src/main/kotlin/eu/nitok/jitsu/compiler/cli/Cli.kt
+++ cli/src/main/kotlin/eu/nitok/jitsu/compiler/cli/Cli.kt
@@ -1,24 +1,15 @@
 package eu.nitok.jitsu.compiler.cli
 
-import eu.nitok.jitsu.compiler.cli.commands.Compile
-import eu.nitok.jitsu.compiler.cli.commands.Parse
-import eu.nitok.jitsu.compiler.cli.commands.Process
-import eu.nitok.jitsu.compiler.cli.commands.Transpile
 import picocli.CommandLine
 import picocli.CommandLine.*
 import picocli.CommandLine.Model.CommandSpec
-import java.io.File
-import java.nio.file.Paths
 import kotlin.system.exitProcess
 
 
 @Command(
     name="jitsu",
     scope = ScopeType.INHERIT,
-    synopsisSubcommandLabel = "COMMAND", subcommands = [
-        Parse::class, Compile::class, Process::class,
-        Transpile::class
-    ]
+    synopsisSubcommandLabel = "COMMAND", subcommands = []
 )
 class Jitsu : Runnable {
     @Spec
